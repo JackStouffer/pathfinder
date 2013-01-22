aa119ebfc72dd2fb660db8774da1a6e0265b82b4
Jack Stouffer <stoufferjack@gmail.com>
2013-01-22 09:42:32 -0500

    Added public domain textures from opengameart. Added in perlin noise terrain generation from code from middlerun on the forums, added in pickle for table sterilization for game saving.

--- map.lua
+++ map.lua
@@ -46,25 +46,25 @@ map = {
 }
 
 -- map variables
-map_w =table.getn(map)
-map_h = table.getn(map[1])
+map_w = 500
+map_h = 500
 map_x = 0
 map_y = 0
-map_offset_x = -16 --to make the map appear at the edge of the screen and not have a black border
-map_offset_y = -16 --to make the map appear at the edge of the screen and not have a black border
+map_offset_x = -32 --to make the map appear at the edge of the screen and not have a black border
+map_offset_y = -32 --to make the map appear at the edge of the screen and not have a black border
 map_display_w = 64
 map_display_h = 44
-tile_w = 16
-tile_h = 16
+tile_w = 32
+tile_h = 32
+Number_Of_Octaves = 8
+persistence = .25
+rect = love.graphics.newImage("textures/square.png")
 
 function drawMap()
    --legacy code is bad M'kay, need to replace this
    for y=1, map_display_h do
       for x=1, map_display_w do                                                         
-         love.graphics.draw( 
-            tile[map[y+map_y][x+map_x]], 
-            (x*tile_w)+map_offset_x, 
-            (y*tile_h)+map_offset_y )
+         love.graphics.draw(tile[map[y+map_y][x+map_x]], (x*tile_w)+map_offset_x, (y*tile_h)+map_offset_y)
       end
    end
 end
@@ -73,8 +73,8 @@ function testCollisionTile(x, y)
     -- if the tile is rock or water, return true
     -- I add an extra one due to the way lua handles table indexing with zero not being the first
     -- number, what a stupid design decision
-    if  map[player.y/16 + 1 + y][player.x/16 + 1 + x] == 2 or 
-        map[player.y/16 + 1 + y][player.x/16 + 1 + x] == 3 then
+    if  map[player.y/32 + 1 + y][player.x/32 + 1 + x] == 2 or 
+        map[player.y/32 + 1 + y][player.x/32 + 1 + x] == 3 then
         return true
     end
     return false
@@ -84,67 +84,203 @@ end
 function testMapEdge(x, y)
     --if the player's input is going to send him off the map, then return true
     if  player.x + x < 0 or
-        player.x + x == 1024 or
+        player.x + x == 2048 or
         player.y + y < 0 or
-        player.y + y == 704 then
+        player.y + y == 1408 then
         return true
     end
     return false
 end
 
-function Interpolate(float x0, float x1, float alpha)
-    --linear interpolation
-    return x0 * (1 - alpha) + alpha * x1;
+--Props go to middlerun on the LOVE forums for this awesome perlin code that
+--is much better than what I was making
+
+-- takes table of L values and returns N*(L-3) interpolated values
+function interpolate1D(values, N)
+  newData = {}
+  for i = 1, #values - 3 do
+    P = (values[i+3] - values[i+2]) - (values[i] - values[i+1])
+    Q = (values[i] - values[i+1]) - P
+    R = (values[i+2] - values[i])
+    S = values[i+1]
+    for j = 0, N-1 do
+      x = j/N
+      table.insert(newData, P*x^3 + Q*x^2 + R*x + S)
+    end
+  end
+  return newData
 end
 
-function GenerateWhiteNoise(width, height)
-    noise = {}
-    for x=1,width do
-        noise[x] = {}
-        for y=1,height do
-            noise[x][y] = math.random()
-        end
-    end
-    return noise
+function perlinComponent1D(seed, length, N, amplitude)
+  rawData = {}
+  finalData = {}
+  for i = 1, math.ceil(length/N) + 3 do
+    rawData[i] = amplitude * rand(seed, i)
+  end
+  interpData = interpolate1D(rawData, N)
+  assert(#interpData >= length)
+  for i = 1, length do
+    finalData[i] = interpData[i]
+  end
+  return finalData
 end
 
-function GenerateSmoothNoise(baseNoise, octave)
-{
-    width = getn(baseNoise)
-    height = getn(baseNoise[1])
-    smoothnoise = {}
-    for x=1,width do
-        noise[x] = {}
-        for y=1,height do
-            noise[x][y] = 0
-        end
+function perlin1D(seed, length, persistence, N, amplitude)
+  data = {}
+  for i = 1, length do
+    data[i] = 0
+  end
+  for i = N, 1, -1 do
+    compInterp = 2^(i-1)
+    compAmplitude = amplitude * persistence^(N-i)
+    comp = perlinComponent1D(seed+i, length, compInterp, compAmplitude)
+    for i = 1, length do
+      data[i] = data[i] + comp[i]
     end
- 
-   samplePeriod = 2 ^ octave 
-   sampleFrequency = 1.0 / samplePeriod
- 
-   for i = 1, width do
-        --calculate the horizontal sampling indices
-        sample_i0 = (i / samplePeriod) * samplePeriod;
-        sample_i1 = (sample_i0 + samplePeriod) % width; --wrap around
-        horizontal_blend = (i - sample_i0) * sampleFrequency;
- 
-        for j = 1, height do
-            --calculate the vertical sampling indices
-            sample_j0 = (j / samplePeriod) * samplePeriod;
-            sample_j1 = (sample_j0 + samplePeriod) % height; --wrap around
-            vertical_blend = (j - sample_j0) * sampleFrequency;
- 
-            --blend the top two corners
-            top = Interpolate(baseNoise[sample_i0][sample_j0], baseNoise[sample_i1][sample_j0], horizontal_blend);
- 
-            --blend the bottom two corners
-            bottom = Interpolate(baseNoise[sample_i0][sample_j1], baseNoise[sample_i1][sample_j1], horizontal_blend);
- 
-            --final blend
-            smoothNoise[i][j] = Interpolate(top, bottom, vertical_blend);
-        end
-   end
- 
-   return smoothNoise;
+  end
+  return data
+end
+
+function interpolate2D(values, N)
+  newData1 = {}
+  for r = 1, #values do
+    newData1[r] = {}
+    for c = 1, #values[r] - 3 do
+      P = (values[r][c+3] - values[r][c+2]) - (values[r][c] - values[r][c+1])
+      Q = (values[r][c] - values[r][c+1]) - P
+      R = (values[r][c+2] - values[r][c])
+      S = values[r][c+1]
+      for j = 0, N-1 do
+        x = j/N
+        table.insert(newData1[r], P*x^3 + Q*x^2 + R*x + S)
+      end
+    end
+  end
+  
+  newData2 = {}
+  for r = 1, (#newData1-3) * N do
+    newData2[r] = {}
+  end
+  for c = 1, #newData1[1] do
+    for r = 1, #newData1 - 3 do
+      P = (newData1[r+3][c] - newData1[r+2][c]) - (newData1[r][c] - newData1[r+1][c])
+      Q = (newData1[r][c] - newData1[r+1][c]) - P
+      R = (newData1[r+2][c] - newData1[r][c])
+      S = newData1[r+1][c]
+      for j = 0, N-1 do
+        x = j/N
+        newData2[(r-1)*N+j+1][c] = P*x^3 + Q*x^2 + R*x + S
+      end
+    end
+  end
+  
+  return newData2
+end
+
+function perlinComponent2D(seed, width, height, N, amplitude)
+  rawData = {}
+  finalData = {}
+  for r = 1, math.ceil(height/N) + 3 do
+    rawData[r] = {}
+    for c = 1, math.ceil(width/N) + 3 do
+      rawData[r][c] = amplitude * rand(seed+r, c)
+    end
+  end
+  interpData = interpolate2D(rawData, N)
+  assert(#interpData >= height and #interpData[1] >= width)
+  for r = 1, height do
+    finalData[r] = {}
+    for c = 1, width do
+      finalData[r][c] = interpData[r][c]
+    end
+  end
+  return finalData
+end
+
+function perlin2D(seed, width, height, persistence, N, amplitude)
+  data = {}
+  for r = 1, height do
+    data[r] = {}
+    for c = 1, width do
+      data[r][c] = 0
+    end
+  end
+  for i = N, 1, -1 do
+    compInterp = 2^(i-1)
+    compAmplitude = amplitude * persistence^(N-i)
+    comp = perlinComponent2D(seed+i*1000, width, height, compInterp, compAmplitude)
+    for r = 1, height do
+      for c = 1, width do
+        data[r][c] = data[r][c] + comp[r][c]
+      end
+    end
+  end
+  return data
+end
+
+function rand(seed, n)
+  if n <= 0 then return nil end
+  if seed ~= mySeed or lastN < 0 or n <= lastN then
+    mySeed = seed
+    math.randomseed(seed)
+    lastN = 0
+  end
+  while lastN < n do
+    num = math.random()
+    lastN = lastN + 1
+  end
+  return num - 0.5
+end
+
+function plot1D(values)
+  love.graphics.line(0, love.graphics.getHeight()/2 - 200, love.graphics.getWidth(), love.graphics.getHeight()/2 - 200)
+  love.graphics.line(0, love.graphics.getHeight()/2 + 200, love.graphics.getWidth(), love.graphics.getHeight()/2 + 200)
+  for i = 1, #values - 1 do
+    love.graphics.line((i-1)/(#values-1)*love.graphics.getWidth(), love.graphics.getHeight()/2 - values[i] * 400, (i)/(#values-1)*love.graphics.getWidth(), love.graphics.getHeight()/2 - values[i+1] * 400)
+  end
+end
+
+function plot2D(values)
+  for r = 1, #values do
+    for c = 1, #(values[1]) do
+      love.graphics.setColor(128 + 40 * values[r][c], 128 + 40 * values[r][c], 128 + 40 * values[r][c], 255)
+      love.graphics.rectangle("fill", (c-1)/(#(values[1]))*love.graphics.getWidth(), (r-1)/(#values)*love.graphics.getHeight(), love.graphics.getWidth()/#(values[1]), love.graphics.getHeight()/#values)
+    end
+  end
+end
+
+function makeTerrain(seed)
+  terrain = {}
+  print("making terrain")
+  if seed == nil then seed = os.time() end
+  terrain.seed = seed
+  terrain.perlin = perlin2D(seed, 341, 256, 0.55, 7, 1.5)
+  terrain.value = {}
+  for r = 1, #terrain.perlin do
+    terrain.value[r] = {}
+    dirtMargin = (256-r) * 0.01
+    for c = 1, #(terrain.perlin[r]) do
+      value = terrain.perlin[r][c]
+      if r < 128 then
+        value = value + (128 - r) * 0.025
+      end
+      if value > 0.7 then terrain.value[r][c] = air
+      elseif value > 1.6 - dirtMargin or value < -0.8 then terrain.value[r][c] = dirt
+      else terrain.value[r][c] = stone
+      end
+    end
+  end
+  return terrain
+end
+
+function drawTerrain(terrain)
+  for r = 1, #terrain.value do
+    for c = 1, #(terrain.value[1]) do
+      if terrain.value[r][c] ~= air then
+        if terrain.value[r][c] == stone then love.graphics.setColor(163, 163, 163, 255) end
+        if terrain.value[r][c] == dirt then love.graphics.setColor(130, 97, 21, 255) end
+        love.graphics.rectangle("fill", (c-1)/(#(terrain.value[1]))*love.graphics.getWidth(), (r-1)/(#terrain.value)*love.graphics.getHeight(), love.graphics.getWidth()/#(terrain.value[1]), love.graphics.getHeight()/#terrain.value)
+      end
+    end
+  end
 end
