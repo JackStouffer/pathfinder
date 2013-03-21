943144fea564662fa34ad7717d374c461d30c711
Jack Stouffer <stoufferjack@gmail.com>
2013-03-21 09:35:20 -0400

    Changed button from metatable to 30log class. Added cave systems which are data structures of multiple levels each with their own enemies and items. Added cave music

--- entities.lua
+++ entities.lua
@@ -1,15 +1,15 @@
 monster = class{health, image}
 item = class{image}
 
-function monster:__init(health, image)
-    self.gridx, self.gridy = getRandOpenTile(map, mapWidth, mapHeight)
+function monster:__init(health, image, level)
+    self.gridx, self.gridy = getRandOpenTile(cave.map[level], mapWidth, mapHeight)
     self.x = (self.gridx * 32) - 32
     self.y = (self.gridy * 32) - 32
     self.path = nil
     self.health = health
     self.image = love.graphics.newImage(image)
 
-    collisionMap[(self.y / 32) + 1][(self.x / 32) + 1] = 2
+    cave.collisionMap[current_level][(self.y / 32) + 1][(self.x / 32) + 1] = 2
 end
 
 function monster:draw()
@@ -24,28 +24,28 @@ function monster:turn()
     
     if self.distance <= 400 then
         --chase
-        collisionMap[(self.y / 32) + 1][(self.x / 32) + 1] = 0
+        cave.collisionMap[current_level][(self.y / 32) + 1][(self.x / 32) + 1] = 0
         Astar:setInitialNode((self.x / 32) + 1, (self.y / 32) + 1)
         Astar:setFinalNode((player.x / 32) + 1, (player.y / 32) + 1)
         self.path = Astar:getPath()
         if self.path ~= nil then
             self.x = (self.path[2].x * 32) - 32
             self.y = (self.path[2].y * 32) - 32
-            collisionMap[(self.y / 32) + 1][(self.x / 32) + 1] = 2
+            cave.collisionMap[current_level][(self.y / 32) + 1][(self.x / 32) + 1] = 2
         end
-        collisionMap[(self.y / 32) + 1][(self.x / 32) + 1] = 2
+        cave.collisionMap[current_level][(self.y / 32) + 1][(self.x / 32) + 1] = 2
     else
         --wander
     end
 end
 
-function item:__init(image)
-    self.gridx, self.gridy = getRandOpenTile(map, mapWidth, mapHeight)
+function item:__init(image, level)
+    self.gridx, self.gridy = getRandOpenTile(cave.map[level], mapWidth, mapHeight)
     self.x = (self.gridx * 32) - 32
     self.y = (self.gridy * 32) - 32
     self.image = love.graphics.newImage(image)
 
-    collisionMap[(self.y / 32) + 1][(self.x / 32) + 1] = 3
+    cave.collisionMap[current_level][(self.y / 32) + 1][(self.x / 32) + 1] = 3
 end
 
 function item:draw()
--- gui.lua
+++ gui.lua
@@ -1,34 +1,24 @@
+Button = class{text, x, y}
 
-Button = {}
-Button.__index = Button
-
-function Button.create(text,x,y)
-	
-	local temp = {}
-	setmetatable(temp, Button)
-	temp.hover = false -- whether the mouse is hovering over the button
-	temp.click = false -- whether the mouse has been clicked on the button
-	temp.once = false
-	temp.text = text -- the text in the button
-	temp.width = largeFont:getWidth(text)
-	temp.height = largeFont:getHeight()
-	temp.x = x - (temp.width / 2)
-	temp.y = y
-	return temp
-	
+function Button:__init(text,x,y)
+	self.hover = false -- whether the mouse is hovering over the button
+	self.click = false -- whether the mouse has been clicked on the button
+	self.once = false
+	self.text = text -- the text in the button
+	self.width = largeFont:getWidth(text)
+	self.height = largeFont:getHeight()
+	self.x = x - (self.width / 2)
+	self.y = y
 end
 
 function Button:draw()
-	
 	love.graphics.setFont(mediumFont)
 	if self.hover then love.graphics.setColor(255, 255, 255)
 	else love.graphics.setColor(214, 169, 187) end
 	love.graphics.print(self.text, self.x, self.y-self.height)
-	
 end
 
 function Button:update(dt)
-	
 	self.hover = false
 	
 	local x = love.mouse.getX()
@@ -50,12 +40,10 @@ function Button:update(dt)
 end
 
 function Button:mousepressed(x, y, button)
-	
 	if self.hover then
 		love.audio.play(clickSound)
 		return true
 	end
 	
 	return false
-	
 end
